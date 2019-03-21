182ef11ad7352c1e7c71f394d4f3a247dce2ae8b
bagaking <zfaustk@qq.com>
2019-03-21 18:54:19 +0800

    feat: cli done

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,37 @@
 #!/usr/bin/env node
 
-require("..").run("tonartstoken", null, 2000)
+const { batch } = require("..")
+
+const debug = require("debug")
+debug.enable("error:*")
+
+const Yargs = require("yargs")
+const packageObj = require("../package.json")
+
+const argv = Yargs
+    .usage("Usage: eos-action-dumper <accounts> [Options]")
+    .example("eos-action-dumper tonartstoken ctserver2111 --watch 2000")
+    .option("watch", {
+        alias: "w",
+        default: -1,
+        describe: "The watcher's retry time span. If the span is less than 0, only execute once."
+    })
+    .option("url", {
+        alias: "u",
+        default: "mongodb://localhost/actions",
+        describe: "mongo connection string."
+    })
+    .help("h")
+    .alias("h", "help")
+    .alias("v", "version")
+    .version(packageObj.version)
+    .argv
+
+const accounts = (typeof argv["_"] === "string") ? [ argv["_"] ] : argv["_"]
+
+if (accounts.length <= 0) {
+    Yargs.showHelp()
+    process.exit(0)
+}
+
+batch(accounts, argv.url, argv.watch)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import { Dumper } from "./dumper"
+import { important, logInfo } from "./log"
 
 export * from "./db"
 export * from "./dumper"
 export const run =
     async (account, mongoUrl, retryTimeSpan = 10000) =>
         (new Dumper(account, mongoUrl || "mongodb://localhost/actions", retryTimeSpan)).run()
+
+export const batch = async (accounts, mongoUrl, retryTimeSpan = 10000) => {
+    logInfo("dump batch:", important(...accounts))
+    if (accounts.length <= 0) {
+    } else if (accounts.length === 1) {
+        await run(accounts[0], mongoUrl, retryTimeSpan)
+    } else {
+        let count = 0
+        while (true) {
+            console.log("Sync Round ", count++)
+            for (const i in accounts) {
+                await run(accounts[i], mongoUrl, -1)
+            }
+            if (retryTimeSpan < 0) {
+                break
+            }
+        }
+    }
+}
